ab8d831ef4df7a497c1e56dcb76a0979271c4c58
Ilya Lisin <blacklacost@gmail.com>
2022-10-08 22:49:58 +0300

    Local Only Fields

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,1 +1,11 @@
-export const LoggedInRouter = () => <span>Logged In</span>
+import { isLoggedInVar } from '../apollo'
+
+export const LoggedInRouter = () => {
+  const onClick = () => isLoggedInVar(false)
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <button onClick={onClick}>Log Out</button>
+    </div>
+  )
+}
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,1 +1,11 @@
-export const LoggedOutRouter = () => <span>Logged Out</span>
+import { isLoggedInVar } from '../apollo'
+
+export const LoggedOutRouter = () => {
+  const onClick = () => isLoggedInVar(true)
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <button onClick={onClick}>Log In</button>
+    </div>
+  )
+}
