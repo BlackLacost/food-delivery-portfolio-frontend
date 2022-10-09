b7fe29c9c3d21b90e2f0d49a63bc080a8e266c12
Ilya Lisin <blacklacost@gmail.com>
2022-10-09 10:39:36 +0300

    Router

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,59 +1,16 @@
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { isLoggedInVar } from '../apollo'
-
-interface LoginForm {
-  email: string
-  password: string
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { CreateAccount } from '../pages/create-account'
+import { Login } from '../pages/login'
 
 export const LoggedOutRouter = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginForm>()
-
-  const onSubmit: SubmitHandler<LoginForm> = (data) => {
-    console.log(data)
-    isLoggedInVar(true)
-  }
-
-  const onInvalid = () => {
-    console.log("can't create account")
-  }
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register('email', {
-              required: 'This is required',
-              pattern: /^[a-zA-Z0-9._%+-]+@gmail.com/,
-            })}
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="font-bold text-red-600">
-              {errors.email.message}
-            </span>
-          )}
-          {errors.email?.type === 'pattern' && (
-            <span className="font-bold text-red-600">Only gmail allowed</span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register('password', { required: true })}
-            type="password"
-            placeholder="password"
-          />
-        </div>
-        <button type="submit" className="px-4 py-2 bg-yellow-300 text-gray-500">
-          Log In
-        </button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Login />} />
+          <Route path="create-account" element={<CreateAccount />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
