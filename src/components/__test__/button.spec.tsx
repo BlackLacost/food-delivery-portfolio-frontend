8b7d201297e5e49002f1c6b3eed8bf76313bbc5c
Ilya Lisin <blacklacost@gmail.com>
2022-10-15 11:19:36 +0300

    Button Tests

---
+++ src/components/__test__/button.spec.tsx
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react'
+import { Button } from '../button'
+
+describe('<Button />', () => {
+  it('should render OK with props', () => {
+    const { getByText } = render(
+      <Button canClick={true} loading={false}>
+        test
+      </Button>
+    )
+
+    getByText('test')
+  })
+
+  it('should display loading', () => {
+    const { getByText } = render(<Button loading={true}>test</Button>)
+
+    getByText('Loading...')
+  })
+
+  it('should disabled when loading', () => {
+    const { getByText } = render(<Button loading={true}>test</Button>)
+
+    expect(getByText('Loading...').closest('button')).toHaveAttribute(
+      'disabled'
+    )
+  })
+
+  it('should disabled', () => {
+    const { getByText } = render(<Button canClick={false}>test</Button>)
+
+    expect(getByText('test').closest('button')).toHaveAttribute('disabled')
+  })
+
+  it('should enable', () => {
+    const { getByText } = render(<Button canClick={true}>test</Button>)
+
+    expect(getByText('test').closest('button')).not.toHaveAttribute('disabled')
+  })
+})
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -14,7 +14,7 @@ export const Button: FC<Props> = ({
     <button
       className="bg-lime-600 py-4 text-lg font-medium text-white transition-colors hover:bg-lime-700 focus:bg-lime-700 focus:outline-none disabled:bg-gray-300"
       type="submit"
-      disabled={!canClick}
+      disabled={!canClick || loading}
     >
       {loading ? 'Loading...' : children}
     </button>
