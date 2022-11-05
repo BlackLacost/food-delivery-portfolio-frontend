b6b2199f55cc5dd81cf72ce748c6a50a22ed2de6
Ilya Lisin <blacklacost@gmail.com>
2022-11-05 18:40:01 +0300

    Logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
 import { useQuery } from '@apollo/client'
 import { FaUserAlt } from 'react-icons/fa'
+import { IoLogOut } from 'react-icons/io5'
 import { Link } from 'react-router-dom'
+import { authTokenVar, isLoggedInVar } from '../apollo'
+import { LOCALSTORAGE_TOKEN } from '../constants'
 import { Me } from '../routers/LoggedInRouter'
 import { Logo } from './Logo'
 
 export const Header = () => {
   const { data } = useQuery(Me)
+
+  const logout = () => {
+    localStorage.setItem(LOCALSTORAGE_TOKEN, '')
+    authTokenVar(null)
+    isLoggedInVar(false)
+  }
+
   return (
     <>
       {!data?.me.verified && (
@@ -18,9 +28,14 @@ export const Header = () => {
           <Link to="/">
             <Logo className="w-52" />
           </Link>
-          <Link to="/edit-profile">
-            <FaUserAlt size={30} />
-          </Link>
+          <div className="flex items-center space-x-4">
+            <Link to="/edit-profile">
+              <FaUserAlt size={30} />
+            </Link>
+            <button type="button" onClick={logout}>
+              <IoLogOut size={43} />
+            </button>
+          </div>
         </div>
       </header>
     </>
