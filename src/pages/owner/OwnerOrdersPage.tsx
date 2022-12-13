ffd0b713a80818d0f6b7229e4a0c0d607814fc97
Ilya Lisin <blacklacost@gmail.com>
2022-12-13 20:25:02 +0300

    New Restaurant Order Subscription

--- src/pages/owner/OwnerOrdersPage.tsx
+++ src/pages/owner/OwnerOrdersPage.tsx
@@ -12,7 +12,6 @@ const GetRestaurantOrdersRoute_Query = graphql(`
     getRestaurantOrders(input: $input) {
       orders {
         id
-        status
         ...OwnerCard_OrderFragment
       }
     }
@@ -23,7 +22,15 @@ const OwnerOrderUpdates_Subscription = graphql(`
   subscription OwnerOrderUpdates($input: OrderUpdatesInput!) {
     orderUpdates(input: $input) {
       id
-      status
+      ...OwnerCard_OrderFragment
+    }
+  }
+`)
+
+const NewRestaurantOrder_Subscription = graphql(`
+  subscription NewRestaurantOrder {
+    pendingOrders {
+      id
       ...OwnerCard_OrderFragment
     }
   }
@@ -33,81 +40,63 @@ export const OwnerOrdersPage = () => {
   const params = useParams<{ id: string }>()
   const restaurantId = Number(params.id)
 
-  const { data, subscribeToMore } = useQuery(GetRestaurantOrdersRoute_Query, {
-    variables: {
-      input: {
-        restaurantId,
-        statuses: [
-          OrderStatus.Pending,
-          OrderStatus.Cooking,
-          OrderStatus.Cooked,
-        ],
+  const { data: getOrdersData, subscribeToMore: subscribeToMoreOrders } =
+    useQuery(GetRestaurantOrdersRoute_Query, {
+      variables: {
+        input: {
+          restaurantId,
+          statuses: [
+            OrderStatus.Pending,
+            OrderStatus.Cooking,
+            OrderStatus.Cooked,
+          ],
+        },
       },
-    },
-    onError: (error) => notify.error(error.message),
-  })
-
-  const orders = data?.getRestaurantOrders.orders
+      onError: (error) => notify.error(error.message),
+    })
 
   useEffect(() => {
-    orders?.forEach((order) => {
-      subscribeToMore({
+    if (getOrdersData?.getRestaurantOrders) {
+      subscribeToMoreOrders({
+        document: NewRestaurantOrder_Subscription,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev
+          const newOrder = subscriptionData.data.pendingOrders
+          return {
+            getRestaurantOrders: {
+              ...prev.getRestaurantOrders,
+              orders: prev.getRestaurantOrders.orders.length
+                ? [...prev.getRestaurantOrders.orders, newOrder]
+                : [newOrder],
+            },
+          }
+        },
+      })
+    }
+  }, [getOrdersData, subscribeToMoreOrders])
+
+  useEffect(() => {
+    getOrdersData?.getRestaurantOrders.orders?.forEach((order) => {
+      subscribeToMoreOrders({
         document: OwnerOrderUpdates_Subscription,
         variables: { input: { id: order.id } },
         updateQuery: (prev, { subscriptionData }) => {
-          console.log({ prev, subscriptionData })
           if (!subscriptionData.data) return prev
-          const newOrder = subscriptionData.data.orderUpdates
+          const updatedOrder = subscriptionData.data.orderUpdates
           return {
             getRestaurantOrders: {
               ...prev.getRestaurantOrders,
               orders: prev.getRestaurantOrders.orders.map((order) => {
-                return order.id === newOrder.id ? newOrder : order
+                return order.id === updatedOrder.id ? updatedOrder : order
               }),
             },
           }
         },
       })
     })
-  }, [orders, subscribeToMore])
-  // React.useEffect(() => {
-  //   notDeliveredOrders?.forEach((order) => {
-  //     subscribeToMore({
-  //       document: ClientOrderUpdates_Subscription,
-  //       variables: { input: { id: order.id } },
-  //       updateQuery: (prev, { subscriptionData }) => {
-  //         if (!subscriptionData.data) return prev
-  //         const newOrder = subscriptionData.data.orderUpdates
-  //         return {
-  //           getOrders: {
-  //             ...prev.getOrders,
-  //             orders: prev.getOrders.orders.map((order) => {
-  //               return order.id === newOrder.id ? newOrder : order
-  //             }),
-  //           },
-  //         }
-  //       },
-  //     })
-  //   })
-  // }, [notDeliveredOrders, subscribeToMore])
+  }, [getOrdersData, subscribeToMoreOrders])
 
-  // useEffect(() => {
-  //   if (order) {
-  //     subscribeToMore({
-  //       document: OwnerOrderUpdates_Subscription,
-  //       variables: { input: { id: orderId } },
-  //       updateQuery: (prev, { subscriptionData }) => {
-  //         if (!subscriptionData.data) return prev
-  //         const newOrder = subscriptionData.data.orderUpdates
-  //         return {
-  //           getOwnerOrder: { ...prev.getOwnerOrder, order: newOrder },
-  //         }
-  //       },
-  //     })
-  //   }
-  // }, [order, orderId, subscribeToMore])
-
-  // if (!order) return null
+  const orders = getOrdersData?.getRestaurantOrders.orders
 
   return (
     <>
